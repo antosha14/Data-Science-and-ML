c9b2a7a1f4480b789f61eccce04fe3e66b567ef9
Anton Kozel <anton.kozel.97@mail.ru>
2024-01-25 00:58:58 +0300

    c++ initial material

--- C++_intro/main.cpp
+++ C++_intro/main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
-#include <cstdlib> // для system
-using namespace std;
+#include <iostream> //This is import in c++
+#include <string>
+
+//Different integer notations
+//Variable is a named piece of memory, or link to that piece of memory to be percise
+int decimal = 15; // integer takes 4 bytes or more
+int octal = 017;
+int hexadecimal = 0x0f;
+int bynary = 0b00001111;
+
+int sum_of_two_numbers(int firstNumber, int secondNumber){
+    int twoNumbersSum = firstNumber + secondNumber;
+    return twoNumbersSum;
+}
 
 int main() 
-{ 
-    cout << "Hello, world!" << endl;
+{/*
+    int num_1;
+    int num_2;
+    std::cout << "Please type 2 numbers, seoarated by spaces" << std::endl;
+    std::cin >> num_1 >> num_2; 
+    std::cout << sum_of_two_numbers(num_1, num_2) << std::endl;
+*/
+    //cin thinks that space is a separator for a next variable
+    std::string full_name; //we need to import strings in c++
+    std::cout << "Please type your name and surname" << std::endl;
+    std::getline(std::cin,full_name); //in order to read data with spaces from comand line 1st - reading from, 2nd - putting at
+    std::cout << full_name << std::endl;
     return 0; 
 }
