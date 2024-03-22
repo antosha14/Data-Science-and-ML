fd311448cc55d8d5eecd913efb9f29b77f56401c
Anton Kozel <anton.kozel.97@mail.ru>
2024-03-22 23:52:00 +0300

    22.03 learning notes

---
+++ Курсы/Yandex ML_Book/python_for_DS_book.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+np.random.standard_normal()
+# После переменной в интерактивном воркбуке можно поставить вопрос, чтобы получить более подробную информацию о ней
+# Метод .format(var1, var2) на стринг вставит
+
+ones_matrix = np.ones((1, 5))
+print(ones_matrix)
+ones_submatrix_view = ones_matrix[::2, ::2]  # creates a view, not copy
+ones_matrix[::2, ::2] = np.zeros((3, 3))
+ones_submatrix_view
